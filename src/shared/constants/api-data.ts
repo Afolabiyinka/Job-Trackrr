93221fc533552107dfbbb51060076178ec1138a2
Ice Chain <afolabiyinka738@gmail.com>
2026-07-16 20:34:27 +0100

    fix: update googleLogin to include withCredentials and adjust API endpoint configuration

--- src/shared/constants/api-data.ts
+++ src/shared/constants/api-data.ts
@@ -1,4 +1,4 @@
 const testingEndpoint = "http://localhost:5050/api";
 const prodEndpoint = "https://job-trackrr.onrender.com/api";
-
-export { testingEndpoint, prodEndpoint };
+const tempEndpoint = "https://job-trackrr-backend.onrender.com/api"
+export { testingEndpoint, prodEndpoint, tempEndpoint };
